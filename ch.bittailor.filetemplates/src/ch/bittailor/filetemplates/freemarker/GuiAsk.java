a888f3b5fcb2480ac7329f652a9a34f3769f9c4e
fsc <fsc@19d42145-4215-0410-b771-e24f57563a32>
2009-01-29 16:59:20 +0000

    Better String in Ask Dialog.
    git-svn-id: https://bittailor.homeip.net/svn/Code/eclipse/filetemplates/trunk@529 19d42145-4215-0410-b771-e24f57563a32

--- ch.bittailor.filetemplates/src/ch/bittailor/filetemplates/freemarker/GuiAsk.java
+++ ch.bittailor.filetemplates/src/ch/bittailor/filetemplates/freemarker/GuiAsk.java
@@ -20,7 +20,21 @@ public class GuiAsk implements IAsk {
     dialog.syncExec(asker);    
     return asker.getAnswer();
   }
-
+  
+  public static String toReadableKey(String key){
+    if (key.length()==0){
+      return key;
+    }
+    StringBuilder readableKey = new StringBuilder();
+    readableKey.append(Character.toUpperCase(key.charAt(0)));
+    for (int i = 1; i < key.length() ; i++) {
+      if(Character.isUpperCase(key.charAt(i))){
+        readableKey.append(" ");
+      }
+      readableKey.append(key.charAt(i));
+    }
+    return readableKey.toString();
+  }
 }
 
 class AskForAString implements Runnable{
@@ -34,7 +48,7 @@ class AskForAString implements Runnable{
 
   public void run() {
     InputDialog dlg = new InputDialog(Display.getCurrent().getActiveShell(),
-        "File Template Generator", "String value for "+fKey, fKey, null);
+        "File Template Generator", "String Value For: "+GuiAsk.toReadableKey(fKey), fKey, null);
     if (dlg.open() == Window.OK) {
       fAnswer = dlg.getValue();
     }
@@ -57,7 +71,7 @@ class AskForABoolean implements Runnable{
 
   public void run() {
     MessageDialog dialog = new MessageDialog(Display.getCurrent().getActiveShell(),
-        "File Template Generator", null, "Boolean value for "+fKey, 
+        "File Template Generator", null, "Choose: "+GuiAsk.toReadableKey(fKey), 
         MessageDialog.QUESTION, new String[] { "true","false" },0); 
     fAnswer = dialog.open() == 0;
   }
